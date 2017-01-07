71957151207ee65cb0e7aba6a87481907c9a1b08
lim <lim.dongxing@gmail.com>
2017-01-06 22:11:07 -0800

    testing if the arrays are allocating correctly

--- mynn.cu
+++ mynn.cu
@@ -3,8 +3,8 @@
 #include <string.h>
 
 #define LAYERS 3
-#define L1N 40
-#define L1M 40
+#define L1N 5
+#define L1M 7
 #define L2N 40
 #define L2M 40
 #define L3N 40
@@ -21,8 +21,25 @@ struct Layer{
 struct Net{
 	Layer **L;
 };
-int main(){
+void PRINTMATRIX(float **x,int n,int m){
 	int i,j;
+	for(i=0;i<n;i++){
+		for(j=0;j<m;j++){
+			printf("[%i,%i]%.02f\t",i,j,x[i][j]);
+		}
+	}
+	printf("\n");
+}
+void PRINTARRAY(float *x,int sz){
+	int i;
+	for(i=0;i<sz;i++){
+		printf("[%i]%.02f\t",i,*x++);
+	}
+	printf("\n");
+}
+int main(){
+	int i,j,k;
+	float test[L1N*L1M];
 	Net *net;
 	net=(Net *)malloc(sizeof(Net));
 	net->L=(Layer **)malloc(LAYERS*sizeof(Layer *));
@@ -37,4 +54,18 @@ int main(){
 			net->L[i]->L[j]=(float *)malloc(mDim[i]*sizeof(float));
 		}
 	}
+	for(i=0;i<L1N*L1M;i++){
+		test[i]=i;
+	}
+	PRINTARRAY(test,L1N*L1M);
+	PRINTARRAY(test,L1N*L1M);
+	PRINTMATRIX(net->L[0]->L,*net->L[0]->N,*net->L[0]->M);
+	//memcpy(net->L[0]->L,test,L1N*L1M*sizeof(float));
+	k=0;
+	for(i=0;i<*net->L[0]->N;i++){
+		for(j=0;j<*net->L[0]->M;j++){
+			net->L[0]->L[i][j]=k++;
+		}
+	}
+	PRINTMATRIX(net->L[0]->L,*net->L[0]->N,*net->L[0]->M);
 }
