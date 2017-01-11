452c89bf1632eb01246cd0f833c35e0df71bc13c
lim <lim.dongxing@gmail.com>
2017-01-10 17:42:24 -0800

    add ability to scroll subpixel region

--- img.py
+++ img.py
@@ -10,6 +10,28 @@ class MyThread(threading.Thread):
 		super(MyThread, self).__init__()
 		self.image = image
 		self.quit = False
+		self.i=0
+		self.j=0
+		self.updatePixelBuf()
+
+	def updatePixelBuf(self):
+		gtk.gdk.threads_enter()
+		try:
+			self.rootWindow = gtk.gdk.get_default_root_window()
+			self.sz = self.rootWindow.get_size()
+			self.pb = gtk.gdk.Pixbuf(gtk.gdk.COLORSPACE_RGB,False,8,self.sz[0],self.sz[1])
+			self.pb = self.pb.get_from_drawable(self.rootWindow,self.rootWindow.get_colormap(),0,0,0,0,self.sz[0],self.sz[1])
+		finally:
+			gtk.gdk.threads_leave()
+			
+	def setCropped(self,x,y):
+		gtk.gdk.threads_enter()
+		try:
+			tmp=self.pb.subpixbuf(x,y,200,200)
+			self.image.clear()
+			self.image.set_from_pixbuf(tmp)
+		finally:
+			gtk.gdk.threads_leave()
 
 	def update_image(self,img):
 		gtk.gdk.threads_enter()
@@ -20,32 +42,29 @@ class MyThread(threading.Thread):
 			gtk.gdk.threads_leave()
 		return False
 
-	def set_from_pixbuf(self,pb):
+	def set_from_pixbuf(self):
 		gtk.gdk.threads_enter()
 		try:
 			self.image.clear()
-			self.image.set_from_pixbuf(pb)
+			self.image.set_from_pixbuf(self.pb)
 		finally:
 			gtk.gdk.threads_leave()
 		return False
 
 	def run(self):
 		while not self.quit:
-			w = gtk.gdk.get_default_root_window()
-			sz = w.get_size()
-			pb = gtk.gdk.Pixbuf(gtk.gdk.COLORSPACE_RGB,False,8,sz[0],sz[1])
-			pb = pb.get_from_drawable(w,w.get_colormap(),0,0,0,0,sz[0],sz[1])
-			#pb = pb.get_from_drawable(w,w.get_colormap(),0,0,0,0,200,200)
+			#pb = pb.get_from_drawable(self.rootWindow,self.rootWindow.get_colormap(),0,0,0,0,200,200)
 			#pb.scale(pb,0,0,200,200,0,0,1,1,gtk.gdk.INTERP_NEAREST)
 			#pb=pb.scale_simple(200,200,gtk.gdk.INTERP_NEAREST)
-			pb=pb.subpixbuf(0,0,200,200)
-			#pb.save("screenshot.png","png")
-			gobject.idle_add(self.set_from_pixbuf,pb)
-			time.sleep(5)
-#			gobject.idle_add(self.update_image,'san_francisco.jpg')
-#			time.sleep(1)
-#			gobject.idle_add(self.update_image,"screenshot.png")
-#			time.sleep(1)
+			gobject.idle_add(self.setCropped,self.j,self.i)
+			self.j += 1
+			if self.j>=(1920-200):
+				self.j = 0
+				self.i += 200
+				if self.i>=(1080-200):
+					self.i = 0
+			#gobject.idle_add(self.set_from_pixbuf)
+			time.sleep(0.001)
 
 def event(widget,event):
 	print(event)
