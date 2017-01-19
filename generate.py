a22e02549588d5a1bacd9a7f5521ed6fa6c5a772
lim <lim.dongxing@gmail.com>
2017-01-19 10:29:49 -0800

    separate generation of input and output examples

---
+++ generate.py
@@ -0,0 +1,76 @@
+import numpy as np
+from csvWrap import *
+from filt import *
+import extract
+
+def generate(target='xor'):
+	if target=='xor':
+		inp1=np.array([-1,-1]).astype(np.float64)
+		inp2=np.array([-1,+1]).astype(np.float64)
+		inp3=np.array([+1,-1]).astype(np.float64)
+		inp4=np.array([+1,+1]).astype(np.float64)
+		inp=[inp1,inp2,inp3,inp4]
+
+		out1=np.array([-1,-1]).astype(np.float64)
+		out2=np.array([-1,+1]).astype(np.float64)
+		out3=np.array([+1,-1]).astype(np.float64)
+		out4=np.array([+1,+1]).astype(np.float64)
+		out=[out1,out2,out3,out4]
+
+		return [inp,out]
+
+	elif target=='csv':
+
+		NPOS=4
+		NNEG=4
+
+		inp=[]
+		out=[]
+
+		#INPUTS=129600
+		#filt=Filter(1,INPUTS,1,0,255,False)
+		INPUTS=129600/3
+		filt=Filter(1,INPUTS,1,0,255*3,False)
+
+		for i in range(NPOS):
+			filename='verified/pos'+str(i)+'.csv'
+			x=readCSV(filename)
+			t=np.array(map(int,x)).astype(np.float64)
+			t=t.reshape(-1,len(t)/3,3).sum(axis=2)[0]
+			y=filt.insert1D(t)
+			inp.append(y)
+			out.append([-1,+1])
+
+		for i in range(NNEG):
+			filename='verified/neg'+str(i)+'.csv'
+			x=readCSV(filename)
+			t=np.array(map(int,x)).astype(np.float64)
+			t=t.reshape(-1,len(t)/3,3).sum(axis=2)[0]
+			y=filt.insert1D(t)
+			inp.append(y)
+			out.append([+1,-1])
+		print out
+		return [inp,out]
+
+	elif target=='mnist':
+		INPUTS=28*28
+		filt=Filter(1,INPUTS,1,0,255,False)
+		tmp=extract.extract()
+		[[testLabel,testSet],[trainLabel,trainSet]]=tmp
+		inp=[]
+		out=[]
+		for i in testSet:
+			t=np.array(i).astype(np.float64)
+			y=filt.insert1D(t)
+			inp.append(y)
+		for i in testLabel:
+			tmp=[]
+			for j in range(i):
+				tmp.append(-1)
+		#	tmp.append(1)
+		#	for j in range(10-i-1):
+		#		tmp.append(-1)
+			for j in range(10-i):
+				tmp.append(1)
+			out.append(np.array(tmp).astype(np.float64))
+		return [inp,out]
--- py.py
+++ py.py
@@ -8,14 +8,19 @@ from csvWrap import *
 from filt import *
 import extract
 from nn import printInfo
+from generate import *
 
-INPUTS=129600
-INPUTS=129600/3
+[inp,out]=generate('xor')
+
+INPUTS=len(inp[0])
+#INPUTS=129600
+#INPUTS=129600/3
 #INPUTS=2
-INPUTS=(28*28)
+#INPUTS=(28*28)
 
-OUTPUTS=2
-OUTPUTS=10
+OUTPUTS=len(out[0])
+#OUTPUTS=2
+#OUTPUTS=10
 
 BATCHSIZE=20
 
@@ -28,66 +33,6 @@ PRINTFREQ=10
 GPU=True
 t0=time.clock()
 
-inp1=np.array([-1,-1]).astype(np.float64)
-inp2=np.array([-1,+1]).astype(np.float64)
-inp3=np.array([+1,-1]).astype(np.float64)
-inp4=np.array([+1,+1]).astype(np.float64)
-inp=[inp1,inp2,inp3,inp4]
-
-out1=np.array([-1,-1]).astype(np.float64)
-out2=np.array([-1,+1]).astype(np.float64)
-out3=np.array([+1,-1]).astype(np.float64)
-out4=np.array([+1,+1]).astype(np.float64)
-out=[out1,out2,out3,out4]
-
-NPOS=4
-NNEG=4
-
-inp=[]
-out=[]
-
-#filt=Filter(1,INPUTS,1,0,255,False)
-#filt=Filter(1,INPUTS,1,0,255*3,False)
-filt=Filter(1,INPUTS,1,0,255,False)
-
-for i in range(NPOS):
-	filename='verified/pos'+str(i)+'.csv'
-	x=readCSV(filename)
-	t=np.array(map(int,x)).astype(np.float64)
-	t=t.reshape(-1,len(t)/3,3).sum(axis=2)[0]
-	y=filt.insert1D(t)
-	inp.append(y)
-	out.append([-1,+1])
-
-for i in range(NNEG):
-	filename='verified/neg'+str(i)+'.csv'
-	x=readCSV(filename)
-	t=np.array(map(int,x)).astype(np.float64)
-	t=t.reshape(-1,len(t)/3,3).sum(axis=2)[0]
-	y=filt.insert1D(t)
-	inp.append(y)
-	out.append([+1,-1])
-print out
-inp=[inp1,inp2,inp3,inp4]
-out=[out1,out2,out3,out4]
-tmp=extract.extract()
-[[testLabel,testSet],[trainLabel,trainSet]]=tmp
-inp=[]
-out=[]
-for i in testSet:
-	t=np.array(i).astype(np.float64)
-	y=filt.insert1D(t)
-	inp.append(y)
-for i in testLabel:
-	tmp=[]
-	for j in range(i):
-		tmp.append(-1)
-#	tmp.append(1)
-#	for j in range(10-i-1):
-#		tmp.append(-1)
-	for j in range(10-i):
-		tmp.append(1)
-	out.append(np.array(tmp).astype(np.float64))
 nExamples=len(inp)
 
 np.set_printoptions(precision=4)
@@ -96,7 +41,6 @@ NN=nn.Network(LAYERDIM,GAMMA)
 for epoch in range(EPOCHS):
 	bInp=[]
 	bOut=[]
-	print(epoch)
 	if BATCHSIZE>1:
 		for i in range(BATCHSIZE):
 			r=np.random.randint(0,nExamples)
@@ -104,22 +48,17 @@ for epoch in range(EPOCHS):
 			bOut.append(out[r])
 		[output,error]=NN.batchTrain(bInp,bOut)
 		if (epoch%PRINTFREQ==0):
+			print('epoch:'+str(epoch))
 			for i in range(BATCHSIZE):
 				printInfo(error[i],output[i],bOut[i])
 	else:
 		r=np.random.randint(0,nExamples)
 		[output,error]=NN.train(inp[r],out[r])
 		if (epoch%PRINTFREQ==0):
+			print('epoch:'+str(epoch))
 			printInfo(error,output,out[r])
 for r in range(nExamples):
 	[output,error]=NN.train(inp[r],out[r])
 	printInfo(error,output,out[r])
 tf=time.clock()
 print('elapsed time: '+str(tf-t0)+'s')
-def printInfo(error,output,target):
-	print('error:'),
-	print(error)
-	print('output:'),
-	print(output)
-	print('target:'),
-	print(target)
