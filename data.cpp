57ad83cadfd51192fa454258bdaaa58e9be57c1c
Melvin Lim <melvinlim@MacBook.local>
2018-04-30 14:23:12 -0700

    verify both headers

--- data.cpp
+++ data.cpp
@@ -44,10 +44,17 @@ XorData::XorData():Data(){
 MNISTData::~MNISTData(){}
 MNISTData::MNISTData():Data(){
 	struct idx1 *idx1Header;
-	int fd=open("t10k-labels-idx1-ubyte",O_RDONLY);
-	assert(fd>=0);
-	idx1Header=(struct idx1 *)mmap(0,1024*1024,PROT_READ,MAP_FILE|MAP_SHARED,fd,0);
+	struct idx3 *idx3Header;
+	int fd1=open("t10k-labels-idx1-ubyte",O_RDONLY);
+	int fd3=open("t10k-images-idx3-ubyte",O_RDONLY);
+	assert(fd1>=0);
+	assert(fd3>=0);
+	idx1Header=(struct idx1 *)mmap(0,1024*1024,PROT_READ,MAP_FILE|MAP_SHARED,fd1,0);
+	idx3Header=(struct idx3 *)mmap(0,1024*1024,PROT_READ,MAP_FILE|MAP_SHARED,fd3,0);
 	assert(idx1Header!=MAP_FAILED);
+	assert(idx3Header!=MAP_FAILED);
 	assert(bswap_32(idx1Header->magic)==0x801);
+	assert(bswap_32(idx3Header->magic)==0x803);
 	printf("0x%x\n%d\n",bswap_32(idx1Header->magic),bswap_32(idx1Header->number));
+	printf("0x%x\n%d\n",bswap_32(idx3Header->magic),bswap_32(idx3Header->nImages));
 }
